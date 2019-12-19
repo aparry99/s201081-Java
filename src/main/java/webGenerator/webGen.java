d3df0406a99121a8e2a664d681f850e355c469d2
Andrew Parry <andrew.3.parry@bt.com>
2019-12-19 15:06:12 +0000

    General updates to p2
    General updates to p2

--- src/main/java/webGenerator/webGen.java
+++ src/main/java/webGenerator/webGen.java
@@ -9,22 +9,17 @@ import java.util.Map;
 
 public class webGen {
 	public static void main(String[] args) {
-		Map<String, String> template = new HashMap<>();
-		
+		if (args.length != 3) {
+			System.out.println("usage webGen destination source templates");
+			System.exit(2);
+		}
 		File destination = new File(args[0]);
 		File source = new File(args[1]);
 		File templates = new File(args[2]);
+		webGen webgen = new webGen();
+		webgen.process(destination, source, templates);
 		
-		if (!destination.isDirectory() || !source.isDirectory() || !templates.isDirectory()) {
-			System.out.println("Not a directory!");
-			//do exit
-		}
 		
-		File[] files = templates.listFiles();
-		
-		for (File child : files) {
-			System.out.println(child.getName());
-		}
 		
 		//source, destination, templates
 		
@@ -36,6 +31,19 @@ public class webGen {
 		
 		//templates = where templates are stored
 		// folder with any text format
+	}
+	
+	public void process(File destination, File source, File templates) {
+		if (!destination.isDirectory() || !source.isDirectory() || !templates.isDirectory()) {
+			System.out.println("Not a directory!");
+			//do exit
+		}
+		
+		File[] files = templates.listFiles();
+		
+		for (File child : files) {
+			System.out.println(child.getName());
+		}
 	}
 	
 	//templates, loop through getting templates
