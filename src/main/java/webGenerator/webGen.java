c230c9c558490f2aa12e81f2e823714a050fb22b
Andrew Parry <andrew.3.parry@bt.com>
2019-12-23 15:22:04 +0000

    slight changes
    gson implementation

--- src/main/java/webGenerator/webGen.java
+++ src/main/java/webGenerator/webGen.java
@@ -30,6 +30,7 @@ public class webGen {
 		TemplateProcessor processor = new ReplaceTemplateText();
 		
 		processor.loadTemplates(webgen.processFiles(templates));
+		processor.loadTemplates(webgen.processContext(source));
 		
 		//source, destination, templates
 		
@@ -86,10 +87,65 @@ public class webGen {
 		
 	}
 	
-	public static getContext() {
-		
+	class Response {
+		Map<String, App> descriptor;
+		//get set
 	}
 	
+	class App {
+		String name;
+		int age;
+		String[] messages;
+		//get set
+	}
+
+	public Map<String, String> processContext(File source) {
+		if (!source.isDirectory()) {
+			System.out.println("Not a directory!");
+			System.exit(2);
+		}
+		
+		File[] files = source.listFiles();
+		
+		
+		
+		Map<String, String> newContext = new HashMap<>();
+		
+		//https://www.mkyong.com/java/how-to-read-file-from-java-bufferedreader-example/
+		
+		for (File child : files) {
+			if (!child.isDirectory()) {
+				//find out why buffered reader
+				//read file line by line, puts into newTemplates via string
+				try (BufferedReader buffread = new BufferedReader (new FileReader(child.getPath()))) {
+					StringBuilder stringbuild = new StringBuilder();
+					String line = buffread.readLine();
+					
+					Gson gson = new Gson();
+					Response response = gson.fromJson(myJson, Response.class);
+					
+					//loop through each line
+					while (line != null) {
+						stringbuild.append(line);
+						stringbuild.append(System.lineSeparator());
+						line = buffread.readLine();
+					}
+					String everything = stringbuild.toString();
+					newContext.put(child.getName(), everything);
+				}
+				catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+			else {
+				System.out.println("Not a file");
+			}
+			System.out.println(newContext);
+		}
+		return newContext;
+		
+	}
+
 	
 	
 	//templates, loop through getting templates
